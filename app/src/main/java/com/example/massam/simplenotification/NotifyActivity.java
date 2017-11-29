c4a804e0fcd04a70bb106aa39dc714eb430e008c
mazsam <arifinsam.sam@gmail.com>
2017-11-29 15:28:44 +0700

    update file Notification

--- app/src/main/java/com/example/massam/simplenotification/NotifyActivity.java
+++ app/src/main/java/com/example/massam/simplenotification/NotifyActivity.java
@@ -3,7 +3,6 @@ package com.example.massam.simplenotification;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 public class NotifyActivity extends AppCompatActivity {
 
     @Override
